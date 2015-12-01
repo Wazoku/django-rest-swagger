3870773bf4a651407ba2dd931bbb0972915e7389
Anton Kirilenko <anton.kirilenko@wazoku.com>
2015-12-01 14:50:37 +0000

    Public API versioning support

--- rest_framework_swagger/__init__.py
+++ rest_framework_swagger/__init__.py
@@ -1,5 +1,6 @@
 VERSION = '0.3.4'
 
+
 DEFAULT_SWAGGER_SETTINGS = {
     'exclude_namespaces': [],
     'api_version': '',
@@ -14,29 +15,38 @@ DEFAULT_SWAGGER_SETTINGS = {
     'resource_access_handler': None,
     'template_path': 'rest_framework_swagger/index.html',
     'doc_expansion': 'none',
+    'version_resolver': 'rest_framework_swagger.fake_version_resolver',
 }
 
-try:
-    from django.conf import settings
-    from django.test.signals import setting_changed
+from django.conf import settings
+from django.test.signals import setting_changed
 
-    def load_settings(provided_settings):
-        global SWAGGER_SETTINGS
-        SWAGGER_SETTINGS = provided_settings
+def load_settings(provided_settings):
+    global SWAGGER_SETTINGS
+    SWAGGER_SETTINGS = provided_settings
 
-        for key, value in DEFAULT_SWAGGER_SETTINGS.items():
-            if key not in SWAGGER_SETTINGS:
-                SWAGGER_SETTINGS[key] = value
+    for key, value in DEFAULT_SWAGGER_SETTINGS.items():
+        if key not in SWAGGER_SETTINGS:
+            SWAGGER_SETTINGS[key] = value
 
-    def reload_settings(*args, **kwargs):
-        setting, value = kwargs['setting'], kwargs['value']
-        if setting == 'SWAGGER_SETTINGS':
-            load_settings(value)
+def reload_settings(*args, **kwargs):
+    setting, value = kwargs['setting'], kwargs['value']
+    if setting == 'SWAGGER_SETTINGS':
+        load_settings(value)
 
-    load_settings(getattr(settings,
-                          'SWAGGER_SETTINGS',
-                          DEFAULT_SWAGGER_SETTINGS))
-    setting_changed.connect(reload_settings)
+load_settings(getattr(settings,
+                      'SWAGGER_SETTINGS',
+                      DEFAULT_SWAGGER_SETTINGS))
+setting_changed.connect(reload_settings)
 
-except:
-    SWAGGER_SETTINGS = DEFAULT_SWAGGER_SETTINGS
+
+class FakeVersionResolver(object):
+    @staticmethod
+    def parse_version_string(value):
+        return 1, 0
+
+    @property
+    def available_versions(self):
+        return [(1, 0)]
+
+fake_version_resolver = FakeVersionResolver()
--- rest_framework_swagger/views.py
+++ rest_framework_swagger/views.py
@@ -6,6 +6,8 @@ from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_text
 from django.shortcuts import render_to_response, RequestContext
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 from .compat import import_string
 
 from rest_framework.views import Response
@@ -52,23 +54,61 @@ def get_full_base_path(request):
 
 class SwaggerUIView(View):
     def get(self, request, *args, **kwargs):
-
         if not self.has_permission(request):
             return self.handle_permission_denied(request)
+
+        version_resolver = import_string(rfs.SWAGGER_SETTINGS['version_resolver'])
+
+        version = kwargs.get('version')
+        if version:
+            version = version_resolver.parse_version_string(version)
+
+            # Check version is available
+            if version not in version_resolver.available_versions:
+                version = None
+
+        # In case of missing or invalid version - redirect to the latest version docs.
+        if not version:
+            latest_version = max(version_resolver.available_versions)
+            return redirect(
+                reverse(
+                    'django.swagger.base.view__versioned',
+                    kwargs=dict(version='.'.join(map(str, latest_version))),
+                ),
+            )
+
+        version_string = '%s.%s' % version
+
+        available_versions = []
+
+        for ver in sorted(version_resolver.available_versions):
+            ver_str = '%s.%s' % ver
+            available_versions.append({
+                'version': ver_str,
+                'is_current': ver == version,
+                'link': "/v%s/" % ver_str,
+            })
 
         template_name = rfs.SWAGGER_SETTINGS.get('template_path')
         data = {
             'swagger_settings': {
-                'discovery_url': "%s/api-docs/" % get_full_base_path(request),
+                'discovery_url': "%s/api-docs/v%s/" % (
+                    get_full_base_path(request),
+                    version_string,
+                ),
                 'api_key': rfs.SWAGGER_SETTINGS.get('api_key', ''),
                 'token_type': rfs.SWAGGER_SETTINGS.get('token_type'),
                 'enabled_methods': mark_safe(
                     json.dumps(rfs.SWAGGER_SETTINGS.get('enabled_methods'))),
                 'doc_expansion': rfs.SWAGGER_SETTINGS.get('doc_expansion', ''),
-            }
+            },
+            'version': version_string,
+            'available_versions': available_versions,
         }
         response = render_to_response(
-            template_name, RequestContext(request, data))
+            template_name,
+            RequestContext(request, data),
+        )
 
         return response
 
@@ -99,12 +139,12 @@ class SwaggerUIView(View):
 class SwaggerResourcesView(APIDocView):
     renderer_classes = (JSONRenderer, )
 
-    def get(self, request):
+    def get(self, request, version):
         apis = [{'path': '/' + path} for path in self.get_resources()]
         return Response({
             'apiVersion': rfs.SWAGGER_SETTINGS.get('api_version', ''),
             'swaggerVersion': '1.2',
-            'basePath': self.get_base_path(),
+            'basePath': self.get_base_path(version=version),
             'apis': apis,
             'info': rfs.SWAGGER_SETTINGS.get('info', {
                 'contact': '',
@@ -116,7 +156,7 @@ class SwaggerResourcesView(APIDocView):
             }),
         })
 
-    def get_base_path(self):
+    def get_base_path(self, version):
         try:
             base_path = rfs.SWAGGER_SETTINGS['base_path']
         except KeyError:
@@ -124,25 +164,31 @@ class SwaggerResourcesView(APIDocView):
                 self.request.path).rstrip('/')
         else:
             protocol = 'https' if self.request.is_secure() else 'http'
-            return '{0}://{1}/{2}'.format(protocol, base_path, 'api-docs')
+            return '{0}://{1}/{2}/v{3}'.format(protocol, base_path, 'api-docs', version)
 
     def get_resources(self):
         urlparser = UrlParser()
         urlconf = getattr(self.request, "urlconf", None)
         exclude_namespaces = rfs.SWAGGER_SETTINGS.get('exclude_namespaces')
-        apis = urlparser.get_apis(urlconf=urlconf, exclude_namespaces=exclude_namespaces)
+
+        apis = urlparser.get_apis(
+            urlconf=urlconf,
+            exclude_namespaces=exclude_namespaces,
+            version=self.version,
+        )
         authorized_apis = filter(lambda a: self.handle_resource_access(self.request, a['pattern']), apis)
-        authorized_apis_list = list(authorized_apis)
-        resources = urlparser.get_top_level_apis(authorized_apis_list)
-        return resources
+        return urlparser.get_top_level_apis(list(authorized_apis))
 
 
 class SwaggerApiView(APIDocView):
     renderer_classes = (JSONRenderer, )
 
-    def get(self, request, path):
+    def get(self, request, version, path):
         apis = self.get_apis_for_resource(path)
-        generator = DocumentationGenerator(for_user=request.user)
+        generator = DocumentationGenerator(
+            for_user=request.user,
+            version=self.version,
+        )
         return Response({
             'apiVersion': rfs.SWAGGER_SETTINGS.get('api_version', ''),
             'swaggerVersion': '1.2',
@@ -155,7 +201,10 @@ class SwaggerApiView(APIDocView):
     def get_apis_for_resource(self, filter_path):
         urlparser = UrlParser()
         urlconf = getattr(self.request, "urlconf", None)
-        apis = urlparser.get_apis(urlconf=urlconf, filter_path=filter_path)
+        apis = urlparser.get_apis(
+            urlconf=urlconf,
+            filter_path=filter_path,
+            version=self.version,
+        )
         authorized_apis = filter(lambda a: self.handle_resource_access(self.request, a['pattern']), apis)
-        authorized_apis_list = list(authorized_apis)
-        return authorized_apis_list
+        return list(authorized_apis)
