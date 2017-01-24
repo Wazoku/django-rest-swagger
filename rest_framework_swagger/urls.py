95094b26faf3859ce4799a36a41737a455f7a9a2
tomek <tomasz.kaputa@wazoku.com>
2017-01-24 16:41:41 +0000

    Updated swagger urls so that they're compatible with Django 1.8 and above

--- rest_framework_swagger/urls.py
+++ rest_framework_swagger/urls.py
@@ -1,12 +1,26 @@
-from django.conf.urls import patterns
 from django.conf.urls import url
-from rest_framework_swagger.views import SwaggerResourcesView, SwaggerApiView, SwaggerUIView
-
-
-urlpatterns = patterns(
-    '',
-    url(r'^$', SwaggerUIView.as_view(), name="django.swagger.base.view"),
-    url(r'^v(?P<version>\d+(\.\d+)?)/$', SwaggerUIView.as_view(), name="django.swagger.base.view__versioned"),
-    url(r'^api-docs/v(?P<version>\d+(\.\d+)?)/$', SwaggerResourcesView.as_view(), name="django.swagger.resources.view"),
-    url(r'^api-docs/v(?P<version>\d+(\.\d+)?)/(?P<path>.*)/?$', SwaggerApiView.as_view(), name='django.swagger.api.view'),
+from rest_framework_swagger.views import (
+    SwaggerResourcesView,
+    SwaggerApiView,
+    SwaggerUIView,
 )
+
+
+urlpatterns = [
+    url(r'^$', SwaggerUIView.as_view(), name="django.swagger.base.view"),
+    url(
+        r'^v(?P<version>\d+(\.\d+)?)/$',
+        SwaggerUIView.as_view(),
+        name="django.swagger.base.view__versioned",
+    ),
+    url(
+        r'^api-docs/v(?P<version>\d+(\.\d+)?)/$',
+        SwaggerResourcesView.as_view(),
+        name="django.swagger.resources.view",
+    ),
+    url(
+        r'^api-docs/v(?P<version>\d+(\.\d+)?)/(?P<path>.*)/?$',
+        SwaggerApiView.as_view(),
+        name='django.swagger.api.view',
+    ),
+]
